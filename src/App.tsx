c387e133f477f3a4b27f03de3bd8c0fd052aa792
Lazaro-Costa <lazarocosta.dev@gmail.com>
2024-01-10 11:41:55 -0300

    Refact: Removido o botao de Login e componentes derivados

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,12 @@ import './App.css';
 import { Header } from './Components/Header';
 import Home from './Components/Home/Home';
 import { Footer } from './Components/Footer';
-import Cadastro from './Components/Cadastro/Cadastro';
 import ProfileIdol from './Components/ProfileIdol/ProfileIdol';
 import ProfileCompany from './Components/ProfileCompany/ProfileCompany';
 import ProfileGroups from './Components/ProfileGroup/ProfileGroup';
-import Login from './Components/Cadastro/Login/Login';
-import { useUserStore } from './UserZustand';
-import React from 'react';
-import ProtectedRouter from './Components/Helper/ProtectedRouter';
 import NotFound from './Components/Helper/NotFound';
-import UpdateProvider from './Components/Update/UpdateProvider';
-function App() {
-  const { autoLogin } = useUserStore();
 
-  React.useEffect(() => {
-    autoLogin();
-  }, []);
+function App() {
   return (
     <div className="App">
       <BrowserRouter>
@@ -33,26 +23,7 @@ function App() {
               key="company"
             />
             <Route path="idol/:id" element={<ProfileIdol />} key="idol" />
-            <Route path="login/*" element={<Login />} key="login" />
-            <Route
-              path="cadastro/*"
-              element={
-                <ProtectedRouter>
-                  <Cadastro />
-                </ProtectedRouter>
-              }
-              key="cadastro"
-            />
-            <Route
-              path="update/*"
-              element={
-                <ProtectedRouter>
-                  <UpdateProvider />
-                </ProtectedRouter>
-              }
-              key="update"
-            />
-            <Route path='*' element={<NotFound />} key='404'/>
+            <Route path="*" element={<NotFound />} key="404" />
           </Routes>
         </main>
         <Footer.Root />
